f14e685f78276db39ccea4858b32cbe2b4d64b77
韩佳宁 <hanjianing@mudu.tv>
2019-10-21 14:57:12 +0800

    主从结构

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,21 +7,18 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+
   selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
-  onSelect(hero: Hero | number): void {
-    if (hero instanceof Hero) {
-      this.selectedHero = hero;
-    } else {
-      console.log(hero);
-    }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
